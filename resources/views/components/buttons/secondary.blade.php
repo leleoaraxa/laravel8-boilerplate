176b7490799b81c0c484f513fa165738f203ab23
Leonardo Marques Borges <leleoaraxa@gmail.com>
2022-02-08 10:34:50 -0300

    Adicionado Componentes v2

---
+++ resources/views/components/buttons/secondary.blade.php
@@ -0,0 +1,4 @@
+<a
+    {{ $attributes->merge(['class' => 'flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8']) }}>
+    {{ $slot }}
+</a>
---
+++ resources/views/components/buttons/transparent.blade.php
@@ -0,0 +1,4 @@
+<a
+    {{ $attributes->merge(['class' => 'flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8']) }}>
+    {{ $slot }}
+</a>
